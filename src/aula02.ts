0e5742da950bd2bf1954251ec8d8a7de6441da6e
RL-Guerra <Guerraraphael88@gmail.com>
2024-10-22 16:33:04 -0300

    first commit

---
+++ src/aula02.ts
@@ -0,0 +1,6 @@
+const nome = "Raphael";
+const canal = "R4PH43L06";
+
+console.log(nome);
+console.log(canal);
+console.log(nome, canal);
---
+++ src/build/aula02.js
@@ -0,0 +1,6 @@
+"use strict";
+const nome = "Raphael";
+const canal = "R4PH43L06";
+console.log(nome);
+console.log(canal);
+console.log(nome, canal);
